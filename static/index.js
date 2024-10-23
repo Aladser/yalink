74cca91ab2b1ca74eaae77eb9882c007e983fa34
Aladser <Aladser@users.noreply.github.com>
2024-10-23 21:46:36 +0900

    добавлена множественная загрузка файлов

--- static/index.js
+++ static/index.js
@@ -31,6 +31,20 @@ if(public_link_row_checkboxes) {
     }));
 }
 
+// кнопка множественной загрузки файлов
+if(multiple_download_button) {
+    multiple_download_button.addEventListener('click', function(e){
+        // поиск кнопок загрузки из строки чекбокса
+        downloadButtons = [];
+        active_public_link_row_checkboxes.forEach(checkbox => {
+            btn = checkbox.closest(".public-link-row").querySelector('.public-link-row__download-link');
+            downloadButtons.push(btn);
+        })
+        downloadFilesSequentially(Array.from(downloadButtons));
+    });
+}
+
+
 /** Переключает видимость dom-элемента
  * @param {*} dom_element DOM-элемента
  * @param {*} value - значение истинности условия
@@ -41,4 +55,22 @@ function switch_visibility (dom_element, value) {
     } else {
         dom_element.classList.add('d-none');
     }
+}
+
+/** замыкание - последовательно загружает файлы */
+function downloadFilesSequentially(links) {
+    let index = 0;
+
+    const downloadNext = () => {
+        if (index < links.length) {
+            let link = links[index];
+            link.click();
+            index++;
+            setTimeout(downloadNext, 1000);
+        } else {
+            active_public_link_row_checkboxes.forEach(checkbox => checkbox.click());
+        }
+    };
+
+    downloadNext();
 }
