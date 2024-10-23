5b40b6b71ad60d0b468c899bb156c1201d78abcf
Aladser <Aladser@users.noreply.github.com>
2024-10-23 11:24:04 +0900

    MainView - добавлена обязательная авторизация при запросе файлов ссылки

--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ import urllib
 from urllib.parse import urlparse
 
 import requests
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 from main.services import get_shared_files_from_public_link
@@ -20,45 +22,52 @@ general_download_api_link_start = 'https://cloud-api.yandex.net/v1/disk/public/r
 class MainView(TemplateView):
     template_name = 'index.html'
 
+    def get(self, request, *args, **kwargs):
+        # перенравление на страницу авторизации, если идет запрос на получение содержимого ссылки
+        if 'link' in self.request.GET and not request.user.is_authenticated:
+            return redirect(reverse("authen:login"))
+
+        return super().get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["search_url"] = os.getenv("DEFAULT_SEARCH_URL") or ""
+
+        if not 'link' in self.request.GET:
+            return context
 
         # получение файлов публичной ссылки
-        if 'link' in self.request.GET:
-            public_link = context["search_url"] = self.request.GET['link']
-            # ссылка на просмотр
-            list_api_link = list_api_link_start + urllib.parse.quote(public_link)
-            # ссылка на загрузку
-            download_api_link = general_download_api_link_start + urllib.parse.quote(public_link)
+        public_link = context["search_url"] = self.request.GET['link']
+        # ссылка на просмотр
+        list_api_link = list_api_link_start + urllib.parse.quote(public_link)
+        # ссылка на загрузку
+        download_api_link = general_download_api_link_start + urllib.parse.quote(public_link)
 
-            # проверка корректности ссылки
-            public_link_components = urlparse(public_link)
-            if public_link_components.netloc != "disk.yandex.ru":
-                context['error'] = "Это не ссылка на Яндекс диск"
-                return context
+        # проверка корректности ссылки
+        public_link_components = urlparse(public_link)
+        if public_link_components.netloc != "disk.yandex.ru":
+            context['error'] = "Это не ссылка на Яндекс диск"
+            return context
 
-            response = requests.get(public_link)
-            if response.status_code != 200:
-                context['error'] = "Ошибка. Код ошибки " + str(response.status_code)
-                return context
+        response = requests.get(public_link)
+        if response.status_code != 200:
+            context['error'] = "Ошибка. Код ошибки " + str(response.status_code)
+            return context
 
-            # проверка ссылки просмотра файлов
-            response = requests.get(list_api_link)
-            if response.status_code == 404:
-                context['error'] = "Ссылка на найдена"
-                return context
-            elif response.status_code == 500:
-                context['error'] = "Неправильная ссылка"
-                return context
-            elif response.status_code != 200:
-                context['error'] = "Ошибка. Код ошибки " + str(response.status_code)
-                return context
+        # проверка ссылки просмотра файлов
+        response = requests.get(list_api_link)
+        if response.status_code == 404:
+            context['error'] = "Ссылка не найдена"
+            return context
+        elif response.status_code == 500:
+            context['error'] = "Неправильная ссылка"
+            return context
+        elif response.status_code != 200:
+            context['error'] = "Ошибка. Код ошибки " + str(response.status_code)
+            return context
 
-            response_data = response.json()
-            items_list = get_shared_files_from_public_link(download_api_link, response_data)
-            context['items'] = items_list
-            context['is_items'] = len(items_list) > 0
-        else:
-            context["search_url"] = os.getenv("DEFAULT_SEARCH_URL") or ""
+        items_list = get_shared_files_from_public_link(download_api_link, response.json())
+        context['items'] = items_list
+        context['is_items'] = len(items_list) > 0
 
         return context
